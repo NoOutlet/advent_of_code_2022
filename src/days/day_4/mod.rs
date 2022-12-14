d18560fbc10a19387a44d76802026bed5135cd86
Jon Anderson <jon.anderson@willowtreeapps.com>
2022-12-14 01:18:46 -0500

    Day 4, Part 2

--- src/days/day_4/mod.rs
+++ src/days/day_4/mod.rs
@@ -8,11 +8,11 @@ fn read_range(range: &str) -> [u32; 2] {
     });
 }
 
-fn range_overlaps(range1: [u32; 2], range2: [u32; 2]) -> bool {
-    return range1[0] <= range2[0] && range1[1] >= range2[1];
-}
-
 pub fn part_1(contents: String) {
+    fn range_overlaps(range1: [u32; 2], range2: [u32; 2]) -> bool {
+        return range1[0] <= range2[0] && range1[1] >= range2[1];
+    }
+
     let overlaps = contents
         .lines()
         .map(|line| {
@@ -27,17 +27,37 @@ pub fn part_1(contents: String) {
         })
         .sum::<u32>();
 
-    println!("{overlaps}");
+    println!("Pairs with full overlap: {overlaps}");
 }
 
-pub fn part_2() {
-    println!("Hello day 4, part 2");
+pub fn part_2(contents: String) {
+    fn range_overlaps(range1: [u32; 2], range2: [u32; 2]) -> bool {
+        return !(range1[1] < range2[0] || range2[1] < range1[0]);
+    }
+
+    let overlaps = contents
+        .lines()
+        .map(|line| {
+            let pair: Vec<&str> = line.split(",").collect();
+            let elf1 = read_range(pair[0]);
+            let elf2 = read_range(pair[1]);
+            if range_overlaps(elf1, elf2) || range_overlaps(elf2, elf1) {
+                return 1;
+            } else {
+                return 0;
+            }
+        })
+        .sum::<u32>();
+
+    println!("Pairs with any overlap: {overlaps}");
 }
 
 pub fn main() {
     let file_path = "src/days/day_4/input.txt";
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
+    println!("Day 4:");
     part_1(contents.clone());
-    part_2();
+    part_2(contents.clone());
+    println!("")
 }
