d6c06615d4fca209aa45daf250f4fd539c788d32
Jon Anderson <jon.anderson@willowtreeapps.com>
2022-12-14 01:06:02 -0500

    Add Day 4 input

--- src/days/day_4/mod.rs
+++ src/days/day_4/mod.rs
@@ -1,12 +1,17 @@
+use std::fs;
+
 pub fn part_1() {
-    println!("Hello day 1, part 1");
+    println!("Hello day 4, part 1");
 }
 
 pub fn part_2() {
-    println!("Hello day 1, part 2");
+    println!("Hello day 4, part 2");
 }
 
 pub fn main() {
+    let file_path = "src/days/day_4/input.txt";
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
     part_1();
     part_2();
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,6 @@ pub mod days;
 fn main() {
     // days::day_1::main();
     // days::day_2::main();
-    days::day_3::main();
+    // days::day_3::main();
+    days::day_4::main();
 }
