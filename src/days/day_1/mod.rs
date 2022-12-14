226386435280c3348d85062a8bcf46c7e1839a4a
Jon Anderson <jon.anderson@willowtreeapps.com>
2022-12-13 23:26:36 -0500

    Day 1, Part 1

--- src/days/day_1/mod.rs
+++ src/days/day_1/mod.rs
@@ -1,5 +1,26 @@
-pub fn part_1() {
-    println!("Hello day 1, part 1");
+use std::fs;
+
+pub fn part_1(contents: String) {
+    let contents: Vec<&str> = contents.split("\n").collect();
+    let mut elves: Vec<u32> = vec![0];
+    let mut index = 0;
+
+    for line in contents {
+        if line.len() == 0 {
+            index += 1;
+        } else {
+            if elves.len() == index {
+                elves.push(0);
+            }
+            let calories: u32 = line.trim().parse().expect("This should be a number");
+            elves[index] += calories;
+        }
+    }
+
+    elves.sort();
+    let calories = &elves[index - 1];
+
+    println!("The highest number of calories is: {calories}");
 }
 
 pub fn part_2() {
@@ -7,6 +28,11 @@ pub fn part_2() {
 }
 
 pub fn main() {
-    part_1();
+    let file_path = "src/days/day_1/input.txt";
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("Day 1:");
+    part_1(contents.clone());
     part_2();
+    println!("");
 }
