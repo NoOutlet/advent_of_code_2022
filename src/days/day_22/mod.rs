666c2026ebe5927d79b6c8e22baede27e464448f
Jon Anderson <jon.anderson@willowtreeapps.com>
2022-12-13 23:04:05 -0500

    Advent of Code organization

---
+++ src/days/day_22/mod.rs
@@ -0,0 +1,12 @@
+pub fn part_1() {
+    println!("Hello day 1, part 1");
+}
+
+pub fn part_2() {
+    println!("Hello day 1, part 2");
+}
+
+pub fn main() {
+    part_1();
+    part_2();
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+pub mod days;
+
+fn main() {
+    days::day_1::main();
+}
