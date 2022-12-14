eb5c56bcd46fcf3b37f149963136320f3a482ff7
Jon Anderson <jon.anderson@willowtreeapps.com>
2022-12-14 01:11:22 -0500

    Day 4, Part 1

--- src/days/day_4/mod.rs
+++ src/days/day_4/mod.rs
@@ -1,7 +1,33 @@
 use std::fs;
 
-pub fn part_1() {
-    println!("Hello day 4, part 1");
+fn read_range(range: &str) -> [u32; 2] {
+    let tuple = range.split_once("-").unwrap();
+    return [tuple.0, tuple.1].map(|s| match s.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 0,
+    });
+}
+
+fn range_overlaps(range1: [u32; 2], range2: [u32; 2]) -> bool {
+    return range1[0] <= range2[0] && range1[1] >= range2[1];
+}
+
+pub fn part_1(contents: String) {
+    let overlaps = contents
+        .lines()
+        .map(|line| {
+            let pair: Vec<&str> = line.split(",").collect();
+            let elf1 = read_range(pair[0]);
+            let elf2 = read_range(pair[1]);
+            if range_overlaps(elf1, elf2) || range_overlaps(elf2, elf1) {
+                return 1;
+            } else {
+                return 0;
+            }
+        })
+        .sum::<u32>();
+
+    println!("{overlaps}");
 }
 
 pub fn part_2() {
@@ -12,6 +38,6 @@ pub fn main() {
     let file_path = "src/days/day_4/input.txt";
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
-    part_1();
+    part_1(contents.clone());
     part_2();
 }
