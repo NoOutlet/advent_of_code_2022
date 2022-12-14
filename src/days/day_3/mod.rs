781a5a7e75e889aa278403b78d15ad0924679d78
Jon Anderson <jon.anderson@willowtreeapps.com>
2022-12-14 01:03:53 -0500

    Day 3, Part 2

--- src/days/day_3/mod.rs
+++ src/days/day_3/mod.rs
@@ -1,8 +1,16 @@
 use std::fs;
 
-pub fn part_1(contents: String) {
+fn get_priority(item: char) -> u32 {
     let priority_map = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return (priority_map.find(item).unwrap() + 1).try_into().unwrap();
+}
 
+fn string_union(word1: String, word2: String) -> String {
+    let shared_letters: Vec<char> = word1.chars().filter(|&char| word2.contains(char)).collect();
+    return String::from_iter(shared_letters);
+}
+
+pub fn part_1(contents: String) {
     let contents: Vec<&str> = contents.split("\n").collect();
     let mut sum = 0;
 
@@ -17,15 +25,36 @@ pub fn part_1(contents: String) {
             .chars()
             .find(|&char| second_compartment.contains(char))
             .unwrap();
-        let priority = priority_map.find(mistake).unwrap() + 1;
+        let priority = get_priority(mistake);
         sum += priority;
     }
 
     println!("Sum of priorities: {sum}");
 }
 
-pub fn part_2() {
-    println!("Hello day 1, part 2");
+pub fn part_2(contents: String) {
+    let contents: Vec<&str> = contents.split("\n").collect();
+    let mut sum = 0;
+
+    let mut index = 0;
+    loop {
+        if (index * 3 + 2) > contents.len() {
+            break;
+        }
+
+        let first_elf = String::from(contents[index * 3]);
+        let second_elf = String::from(contents[index * 3 + 1]);
+        let third_elf = String::from(contents[index * 3 + 2]);
+        let badge = string_union(string_union(first_elf, second_elf), third_elf)
+            .chars()
+            .nth(0)
+            .unwrap();
+        let priority = get_priority(badge);
+        sum += priority;
+        index += 1;
+    }
+
+    println!("Sum of priorities: {sum}");
 }
 
 pub fn main() {
@@ -34,6 +63,6 @@ pub fn main() {
 
     println!("Day 3:");
     part_1(contents.clone());
-    part_2();
+    part_2(contents.clone());
     println!("");
 }
