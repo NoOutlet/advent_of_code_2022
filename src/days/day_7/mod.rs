3ac229140fed64374153aa75ede269dbcbfa3c64
Jon Anderson <jon.anderson@willowtreeapps.com>
2023-01-20 17:55:16 -0500

    Day 7, Part 2

--- src/days/day_7/mod.rs
+++ src/days/day_7/mod.rs
@@ -4,7 +4,7 @@ fn get_path(crumbs: Vec<&str>) -> String {
     return crumbs.join(" ");
 }
 
-pub fn part_1(contents: String) {
+fn get_directory_data(contents: String) -> Vec<(String, u32)> {
     let mut directories: Vec<(String, u32)> = vec![("/".to_string(), 0)];
     let mut breadcrumbs: Vec<&str> = Vec::new();
     contents.lines().for_each(|line| {
@@ -30,6 +30,11 @@ pub fn part_1(contents: String) {
             }
         }
     });
+    return directories;
+}
+
+pub fn part_1(contents: String) {
+    let directories = get_directory_data(contents);
     let mut sum = 0;
     for dir in directories {
         let (_name, direct_size) = dir;
@@ -40,8 +45,19 @@ pub fn part_1(contents: String) {
     println!("Sum of directories with size less than 100,000: {sum}");
 }
 
-pub fn part_2(_contents: String) {
-    println!("Hello day 7, part 2");
+pub fn part_2(contents: String) {
+    const TOTAL_SIZE: u32 = 70000000;
+    const NEEDED_SPACE: u32 = 30000000;
+    let directories = get_directory_data(contents).iter();
+    let root = directories.next().unwrap();
+    let mut current_best = (root.0.as_str(), root.1);
+    let space_to_clear = root.1 - (TOTAL_SIZE - NEEDED_SPACE);
+    for dir in directories {
+        let (_name, direct_size) = dir;
+        if direct_size >= &space_to_clear && direct_size < &current_best.1 {
+            current_best = ("blah", *direct_size);
+        }
+    }
 }
 
 pub fn main() {
