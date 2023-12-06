5b831d79fb6acb107a55505871c3eef4c4a46dee
Jon Anderson <jon.anderson@willowtreeapps.com>
2023-12-06 14:27:55 -0500

    Question 7 didn't compile; updated project

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ pub mod days;
 
 fn main() {
     println!("Advent of Code - 2022");
-    // days::day_1::main();
+    days::day_1::main();
     // days::day_2::main();
     // days::day_3::main();
     // days::day_4::main();
     // days::day_5::main();
     // days::day_6::main();
-    days::day_7::main();
+    // days::day_7::main();
     // days::day_8::main();
     // days::day_9::main();
     // days::day_10::main();
